0521370f81e7f584e3a71613c9cace93bd5a8448
Badnameee <stevenrrobles13@gmail.com>
2025-11-07 16:29:11 -0600

    Updated test for HitboxRenderCircle

--- test/shapes_hitbox.py
+++ test/shapes_hitbox.py
@@ -5,8 +5,7 @@ import math
 import pyglet
 from pyglet.window import Window, key
 from pyglet.graphics import Batch, Group
-from pyglet.shapes import Circle
-from src.shapes import HitboxRender
+from src.shapes import HitboxRender, HitboxRenderCircle
 from src.types import Color
 
 window = Window(640, 480)
@@ -15,55 +14,47 @@ group = Group()
 
 hitbox = HitboxRender.from_rect(100, 100, 100, 50, Color.WHITE, batch, group)
 hitbox2 = HitboxRender.from_rect(300, 300, 100, 50, Color.RED, batch, group)
-circle = Circle(100, 100, 50, color=Color.WHITE.value, batch=batch, group=group)
-circle.visible = False
+circle = HitboxRenderCircle(100, 100, 50, color=Color.WHITE, batch=batch, group=group)
+circle.render.visible = False
 
 mode = 'rect'
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
 	hitbox.move_to(x, y)
-	circle.position = x, y
+	circle.move_to(x, y)
 
 @window.event
 def on_key_press(symbol, modifiers):
 	global mode
 
-	if mode == 'rect':
-		if symbol == key.A:
-			hitbox.anchor_x += 10
-		elif symbol == key.D:
-			hitbox.anchor_x -= 10
-		elif symbol == key.W:
-			hitbox.anchor_y -= 10
-		elif symbol == key.S:
-			hitbox.anchor_y += 10
-		elif symbol == key.LEFT:
-			hitbox.angle -= 0.1
-		elif symbol == key.RIGHT:
-			hitbox.angle += 0.1
-	elif mode == 'circle':
-		if symbol == key.A:
-			circle.anchor_x += 10
-		elif symbol == key.D:
-			circle.anchor_x -= 10
-		elif symbol == key.W:
-			circle.anchor_y -= 10
-		elif symbol == key.S:
-			circle.anchor_y += 10
-		elif symbol == key.LEFT:
-			circle.rotation -= 0.1 * 180 / math.pi
-		elif symbol == key.RIGHT:
-			circle.rotation += 0.1 * 180 / math.pi
+	if symbol == key.A:
+		hitbox.anchor_x -= 10
+		circle.anchor_x -= 10
+	elif symbol == key.D:
+		hitbox.anchor_x += 10
+		circle.anchor_x += 10
+	elif symbol == key.W:
+		hitbox.anchor_y += 10
+		circle.anchor_y += 10
+	elif symbol == key.S:
+		hitbox.anchor_y -= 10
+		circle.anchor_y -= 10
+	elif symbol == key.LEFT:
+		hitbox.angle -= 0.1
+		circle.angle -= 0.1
+	elif symbol == key.RIGHT:
+		hitbox.angle += 0.1
+		circle.angle += 0.1
 
 	if symbol == key.C:
 		mode = 'circle' if mode == 'rect' else 'rect'
 		if mode == 'rect':
 			hitbox.render.visible = True
-			circle.visible = False
+			circle.render.visible = False
 		elif mode == 'circle':
 			hitbox.render.visible = False
-			circle.visible = True
+			circle.render.visible = True
 
 def update(dt):
 	if mode == 'rect':
@@ -72,10 +63,10 @@ def update(dt):
 		else:
 			hitbox.render.opacity = 255
 	elif mode == 'circle':
-		if HitboxRender.circle_collide(circle, hitbox2)[0]:
-			circle.opacity = 128
+		if hitbox2.collide(circle)[0]:
+			circle.render.opacity = 128
 		else:
-			circle.opacity = 255
+			circle.render.opacity = 255
 
 @window.event
 def on_draw():
