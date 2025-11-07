f6d4cd6d310552b83de2e178218eb59c0aa7dc79
Badnameee <stevenrrobles13@gmail.com>
2025-11-07 16:35:25 -0600

    Updated Scene docs

--- src/scene.py
+++ src/scene.py
@@ -7,10 +7,13 @@ from pyglet.graphics import Batch
 class Scene(ABC, EventDispatcher):
 	"""Abstract class for a Scene in the game, inherit to create own scenes.
 	Window object should hold all scenes in window.scenes dictionary
+
+	When inheriting, a batch must be created
 	
 	Dispatches `on_scene_change` (to window) when program wishes to switch scenes.
 
-	`enable` and `disable` run from Window when enabling and disabling scene.
+	`enable` and `disable` run from `Window` class when enabling and disabling scene.
+	These enabled and disable the scene, but rendering is stopped because batch is not rendered
 
 	Use kwargs to attach event handlers.
 	"""
@@ -22,7 +25,7 @@ class Scene(ABC, EventDispatcher):
 
 		Args:
 			name (str): The name of the scene (used to identity scene by name)
-			window (Window): The screen window (for pushing/popping handlers)
+			window (Window): The screen window
 			**kwargs: Event handlers to attach (name=func)
 		"""
 		self.name, self.window = name, window
@@ -35,7 +38,7 @@ class Scene(ABC, EventDispatcher):
 
 	@abstractmethod
 	def enable(self) -> None:
-		"""Enables this scene. (does not enable rendering, it is always enabled)"""
+		"""Enables this scene. (does not enable rendering)"""
 	@abstractmethod
 	def disable(self) -> None:
-		"""Disables this scene. (does not disable rendering, it is always enabled)"""
+		"""Disables this scene. (does not disable rendering)"""
